ee591d3b44348ee1e1ab7790291dfe20fe7fa612
Mack Ward <mackorone@gmail.com>
2021-12-31 21:21:55 -0500

    Simplify path to playlists file

--- src/environment.py
+++ src/environment.py
@@ -2,9 +2,11 @@
 
 import os
 import pathlib
-from typing import Optional
+from typing import NewType, Optional
 
 from external import external
+
+SimplifiedPath = NewType("SimplifiedPath", pathlib.Path)
 
 
 class Environment:
@@ -14,20 +16,24 @@ class Environment:
         return os.getenv(name)
 
     @classmethod
-    def get_prod_playlists_dir(cls) -> pathlib.Path:
-        return cls._get_repo_dir() / "playlists"
+    def get_prod_playlists_dir(cls) -> SimplifiedPath:
+        return cls._simplify(cls._get_repo_dir() / "playlists")
 
     @classmethod
-    def get_test_playlists_dir(cls) -> pathlib.Path:
+    def get_test_playlists_dir(cls) -> SimplifiedPath:
         name = "_playlists/"
         repo_dir = cls._get_repo_dir()
         with open(repo_dir / ".gitignore") as f:
             assert name in f.read().splitlines()
-        return repo_dir / name
+        return cls._simplify(repo_dir / name)
 
     @classmethod
     @external
-    def _get_repo_dir(cls) -> pathlib.Path:
+    def _get_repo_dir(cls) -> SimplifiedPath:
         repo_dir = pathlib.Path(__file__).resolve().parent.parent
         assert repo_dir.name == "spotify-playlist-archive"
-        return pathlib.Path(os.path.relpath(repo_dir))
+        return cls._simplify(repo_dir)
+
+    @classmethod
+    def _simplify(cls, path: pathlib.Path) -> SimplifiedPath:
+        return SimplifiedPath(pathlib.Path(os.path.relpath(path)))
