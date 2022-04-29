67fcd9c65e17ebf6c500ab157e8b4d5c2b058351
Mack Ward <mackorone@gmail.com>
2022-04-29 03:50:29 -0400

    Add RelativeType class

--- src/environment.py
+++ src/environment.py
@@ -2,34 +2,32 @@
 
 import os
 import pathlib
-from typing import NewType
 
 from plants.environment import Environment as PlantsEnvironment
 from plants.external import external
 
-SimplifiedPath = NewType("SimplifiedPath", pathlib.Path)
+
+class RelativePath(pathlib.Path):
+    def __new__(cls, path: pathlib.Path) -> pathlib.Path:
+        return super().__new__(cls, os.path.relpath(path))
 
 
 class Environment:
     @classmethod
-    def get_prod_playlists_dir(cls) -> SimplifiedPath:
-        return cls._simplify(cls._get_repo_dir() / "playlists")
+    def get_prod_playlists_dir(cls) -> RelativePath:
+        return RelativePath(cls._get_repo_dir() / "playlists")
 
     @classmethod
-    def get_test_playlists_dir(cls) -> SimplifiedPath:
-        name = "_playlists/"
+    def get_test_playlists_dir(cls) -> RelativePath:
+        dir_name = "_playlists/"
         repo_dir = cls._get_repo_dir()
         with open(repo_dir / ".gitignore") as f:
-            assert name in f.read().splitlines()
-        return cls._simplify(repo_dir / name)
+            assert dir_name in f.read().splitlines()
+        return RelativePath(repo_dir / dir_name)
 
     @classmethod
     @external
-    def _get_repo_dir(cls) -> SimplifiedPath:
+    def _get_repo_dir(cls) -> pathlib.Path:
         repo_dir = PlantsEnvironment.get_repo_root()
         assert repo_dir.name == "spotify-playlist-archive"
-        return cls._simplify(repo_dir)
-
-    @classmethod
-    def _simplify(cls, path: pathlib.Path) -> SimplifiedPath:
-        return SimplifiedPath(pathlib.Path(os.path.relpath(path)))
+        return repo_dir
