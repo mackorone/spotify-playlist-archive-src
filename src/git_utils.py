5be9b8cfae06f5a252a89338414e1bd4015a4fcd
Mack Ward <mackorone@gmail.com>
2023-09-25 23:39:19 -0700

    [skip ci] Update plants and fix type error

--- src/git_utils.py
+++ src/git_utils.py
@@ -2,7 +2,7 @@
 
 import logging
 import subprocess
-from typing import List, Sequence
+from typing import List, Tuple
 
 from plants.subprocess_utils import SubprocessUtils
 
@@ -12,17 +12,17 @@ logger: logging.Logger = logging.getLogger(__name__)
 class GitUtils:
     @classmethod
     def any_uncommitted_changes(cls) -> bool:
-        result = cls._run(["git", "status", "-s"])
+        result = cls._run(("git", "status", "-s"))
         return bool(result.stdout)
 
     @classmethod
     def get_last_commit_content(cls) -> List[str]:
         """Get files affected by the most recent commit"""
-        result = cls._run(["git", "log", "--name-only", "--pretty=format:", "-1"])
+        result = cls._run(("git", "log", "--name-only", "--pretty=format:", "-1"))
         return result.stdout.splitlines()
 
     @classmethod
-    def _run(cls, args: Sequence[str]) -> "subprocess.CompletedProcess[str]":
+    def _run(cls, args: Tuple[str, ...]) -> "subprocess.CompletedProcess[str]":
         logger.info(f"- Running: {args}")
         result = SubprocessUtils.run(args=args)
         logger.info(f"- Exited with: {result.returncode}")
